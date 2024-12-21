88eae4b15a87f91ee7bb7042610c28cff4308681
YepileF <yepilef@gmail.com>
2024-12-21 13:45:48 -0500

    feat: validation employee fields

--- backend/src/middlewares/validators.js
+++ backend/src/middlewares/validators.js
@@ -1,22 +1,6 @@
 import { body } from "express-validator";
 
-export const validateRegister = [
-  body("name")
-    .trim()
-    .notEmpty()
-    .withMessage("El nombre es obligatorio.")
-    .isLength({ max: 50 })
-    .withMessage("El nombre no puede exceder los 50 caracteres."),
-  body("hireDate")
-    .notEmpty()
-    .withMessage("La fecha de contratación es obligatoria.")
-    .isISO8601()
-    .withMessage("La fecha debe estar en formato ISO8601."),
-  body("salary")
-    .notEmpty()
-    .withMessage("El salario es obligatorio.")
-    .isFloat({ min: 0 })
-    .withMessage("El salario debe ser un número positivo."),
+export const validateLogin = [
   body("email")
     .trim()
     .notEmpty()
@@ -38,24 +22,34 @@ export const validateRegister = [
     .withMessage("La contraseña debe contener al menos un número."),
 ];
 
-export const validateLogin = [
-  body("email")
+export const validateRegister = [
+  ...validateLogin,
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("El nombre es obligatorio.")
+    .isLength({ max: 50 })
+    .withMessage("El nombre no puede exceder los 50 caracteres."),
+  body("hireDate")
+    .notEmpty()
+    .withMessage("La fecha de contratación es obligatoria.")
+    .isISO8601()
+    .withMessage("La fecha debe estar en formato ISO8601."),
+  body("salary")
+    .notEmpty()
+    .withMessage("El salario es obligatorio.")
+    .isFloat({ min: 0 })
+    .withMessage("El salario debe ser un número positivo."),
+];
+
+export const createEmployee = [
+  ...validateRegister,
+  body("role")
     .trim()
     .notEmpty()
     .withMessage("El correo electrónico es obligatorio.")
-    .isEmail()
-    .withMessage("El correo debe ser válido.")
-    .normalizeEmail(),
-  body("password")
-    .trim()
-    .notEmpty()
-    .withMessage("La contraseña es obligatoria.")
-    .isLength({ min: 8 })
-    .withMessage("La contraseña debe tener al menos 8 caracteres.")
-    .matches(/[a-z]/)
-    .withMessage("La contraseña debe contener al menos una letra minúscula.")
-    .matches(/[A-Z]/)
-    .withMessage("La contraseña debe contener al menos una letra mayúscula.")
-    .matches(/[0-9]/)
-    .withMessage("La contraseña debe contener al menos un número."),
+    .isString()
+    .withMessage("Rol debe ser de tipo texto.")
+    .isIn(["admin", "employee"])
+    .withMessage("Rol debería ser 'admin' o 'employee'."),
 ];
