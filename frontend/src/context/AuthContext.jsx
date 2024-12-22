f1040fdeab0b284a82aeb3decc81587e6e2b94a4
YepileF <yepilef@gmail.com>
2024-12-21 23:12:35 -0500

    fix: authContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,21 +1,22 @@
-import { createContext, use, useContext, useReducer } from "react";
+import { createContext, useContext, useReducer } from 'react';
+import PropTypes from 'prop-types';
 
 const initialState = {
-    isAuthenticated: false,
-    user: null,
-    token: null,
+  isAuthenticated: false,
+  user: null,
+  token: null,
 };
 
 const authReducer = (state, action) => {
   switch (action.type) {
-    case "LOGIN":
+    case 'LOGIN':
       return {
         ...state,
         isAuthenticated: true,
         user: action.payload.user,
         token: action.payload.token,
       };
-    case "LOGOUT":
+    case 'LOGOUT':
       return initialState;
     default:
       return state;
@@ -25,15 +26,18 @@ const authReducer = (state, action) => {
 const AuthContext = createContext(initialState);
 
 const AuthProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(authReducer, initialState);
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
-    return (
-        <AuthContext.Provider value={{ state, dispatch }}>
-            {children}
-        </AuthContext.Provider>
-    );
-}
-
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 const useAuth = () => useContext(AuthContext);
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export { AuthProvider, useAuth };
