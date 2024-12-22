2588e40d29d8d0de8f8b28af69c72a028f516e0d
YepileF <yepilef@gmail.com>
2024-12-22 01:39:03 -0500

    feat: cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import http from "http";
+import cors from "cors";
 import router from "./src/routes/index.js";
 
 const app = express();
 const server = http.createServer(app);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(router);
