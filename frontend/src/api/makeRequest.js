6a988a9a110520369fc513e3cdc6d1c92d15ac05
YepileF <yepilef@gmail.com>
2024-12-22 10:38:03 -0500

    feat: module requests api

---
+++ frontend/src/api/makeRequest.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const url = import.meta.env.VITE_BACKEND_URL;
+
+if (!url) {
+  throw new Error('La variable de entorno VITE_BACKEND_URL no está definida.');
+}
+
+export async function makeRequest(endpoint, method, body) {
+  try {
+    const response = await axios({
+      method,
+      url: `${url}${endpoint}`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: body,
+    });
+
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      const errorMessage = error.response.data?.message || 'Algo salió mal';
+      throw new Error(errorMessage);
+    } else if (error.request) {
+      throw new Error('No se recibió respuesta del servidor.');
+    } else {
+      throw new Error(error.message || 'Error desconocido');
+    }
+  }
+}
