9c9989ac48783341db3b7a22cd79cafaee4fbcc0
YepileF <yepilef@gmail.com>
2024-12-22 21:29:25 -0500

    feat: hidde button

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,8 +1,10 @@
+import { useAuth } from '../context/AuthContext';
 import NewRequest from './NewRequest';
 import PageLayout from './PageLayout';
 import RequestsTable from './RequestsTable';
 
 function Dashboard() {
+  const { state } = useAuth();
   const showModal = () => {
     document.getElementById('new_request').showModal();
   };
@@ -12,9 +14,11 @@ function Dashboard() {
       <div className="flex min-h-screen w-full flex-col gap-10 px-28 pt-24">
         <div className="flex w-full justify-between">
           <h1 className="text-start text-3xl font-semibold">Solicitudes</h1>
-          <button className="btn btn-primary btn-sm" onClick={showModal}>
-            Agregar Solicitud
-          </button>
+          {state.user?.role === 'admin' && (
+            <button className="btn btn-primary btn-sm" onClick={showModal}>
+              Agregar Solicitud
+            </button>
+          )}
         </div>
         <RequestsTable />
       </div>
